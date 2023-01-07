5bd6e615fb7a5ec26bc8fb89829fd20a9da32d04
Peter Conrad <p.conrad@protonmail.ch>
2023-01-07 19:39:35 +0100

    Update vuejs-intro to Vue 3

--- vuejs-intro/js/script.js
+++ vuejs-intro/js/script.js
@@ -1,66 +1,70 @@
 /*global Vue*/
-var sample01 = new Vue({
-	el: '#sample01',
-	data: {
-		sampleText: 'Hello!',
-		newText: '',
-		visible: true
+const { createApp } = Vue
+
+const app01 = createApp({
+	data() {
+		return {
+			  sampleText: 'Hello!',
+			  newText: '',
+			  visible: true
+		}
 	},
 	methods: {
-		toggleVisible: function() { this.visible = !this.visible; },
-		reverseText: function() { this.sampleText = this.sampleText.split('').reverse().join(''); },
-		updateText: function() { this.sampleText = this.newText; this.newText = ''; }
+		toggleVisible() { this.visible = !this.visible; },
+		reverseText() { this.sampleText = this.sampleText.split('').reverse().join(''); },
+		updateText() { this.sampleText = this.newText; this.newText = ''; }
 	}
-});
+}).mount('#sample01');
 
-function ListItem(description) {
-	this.description = description;
-	this.editing = false;
-	this.newDescription = '';
-}
+class ListItem {
+	constructor(description) {
+		this.description = description;
+		this.editing = false;
+		this.newDescription = '';
+	}
 
-ListItem.prototype = {
-	constructor: ListItem,
-	startUpdate: function() {
+	startUpdate() {
 		this.newDescription = this.description;
 		this.editing = true;
-	},
-	commitUpdate: function() {
+	}
+
+	commitUpdate() {
 		if (this.editing && this.newDescription) {
 			this.description = this.newDescription;
 			this.newDescription = '';
 			this.editing = false;
 		}
-	},
-	abortUpdate: function() {
+	}
+
+	abortUpdate() {
 		this.newDescription = '';
 		this.editing = false;
 	}
-};
+}
 
-let dynamicItem = {
+const dynamicItem = {
 	props: [ 'item' ],
-	template: '<li class="pa1 lh-copy bt-0 bl-0 br-0 bb b--dashed b--black-50 bg-animate hover-bg-near-white flex items-center"><span class="flex-auto" v-if="!item.editing">{{ item.description }}</span>'
-		+ '<span v-on:click="item.startUpdate()" v-if="!item.editing" class="mr1 pointer dim dn item-control icon icon-edit"></span>'
-		+ '<span v-on:click="$emit(\'remove\')" v-if="!item.editing" class="mr1 pointer dim dn item-control icon icon-delete"></span>'
-		+ '<input class="w-100" v-if="item.editing" v-on:keyup.enter="item.commitUpdate()" v-on:keyup.esc="item.abortUpdate()" v-model="item.newDescription" v-focus>'
+	template: `
+		<li class="pa1 lh-copy bt-0 bl-0 br-0 bb b--dashed b--black-50 bg-animate hover-bg-near-white flex items-center">
+			<span class="flex-auto" v-if="!item.editing">{{ item.description }}</span>
+			<span v-on:click="item.startUpdate()" v-if="!item.editing" class="mr1 pointer dim dn item-control icon icon-edit"></span>
+			<span v-on:click="$emit(\'remove\')" v-if="!item.editing" class="mr1 pointer dim dn item-control icon icon-delete"></span>
+			<input class="w-100" v-if="item.editing" v-on:keyup.enter="item.commitUpdate()" v-on:keyup.esc="item.abortUpdate()" v-model="item.newDescription" v-focus>
+		</li>`
 };
 
-Vue.directive('focus', {
-	inserted: function(el) { el.focus(); }
-})
-
-var sample02 = new Vue({
-	el: '#sample02',
-	data: {
-		list: [
-			new ListItem('ダミー１'),
-			new ListItem('ダミー２')
-		],
-		newItem: ''
-	},
+const app02 = createApp({
+	data() {
+		return {
+			list: [
+				new ListItem('ダミー１'),
+				new ListItem('ダミー２')
+			],
+			newItem: ''
+		}
+	  },
 	methods: {
-		addItem: function() {
+		addItem() {
 			if (this.newItem !== '') {
 				this.list.push(new ListItem(this.newItem));
 				this.newItem = '';
@@ -70,4 +74,8 @@ var sample02 = new Vue({
 	components: {
 		'dynamic-item': dynamicItem
 	}
-});
+})
+.directive('focus', {
+	mounted: (el) => el.focus()
+})
+.mount('#sample02');
